7d03d0b683850dd8fad1003f2f7c03a7015ff365
rajneeshjatt <rajneeshbabal@gmail.com>
2019-12-04 15:38:43 +0530

    comments'

---
+++ src/main/java/test001/test001/App.java
@@ -0,0 +1,13 @@
+package test001.test001;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/main/java/test001/test001/NewTest.java
@@ -0,0 +1,10 @@
+package test001.test001;
+
+import org.testng.annotations.Test;
+
+public class NewTest {
+  @Test
+  public void f() {
+	  System.out.println("hiiiiiii");
+  }
+}
